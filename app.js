061b7bacd8bc45862a9a53db2c4b4ab47f36eb92
Corinna <x.corinna.x@hotmail.co.uk>
2025-08-22 11:25:47 +0100

    following demo for setting up and making steps to follow

--- app.js
+++ app.js
@@ -1,1 +1,26 @@
-console.log("hello worlds");
+console.log("hello world");
+
+//TODO: i need to store my image data
+//images stored locally or images stored remotely (unsplash...)
+//each image is an object (mnost efficient way)
+
+//relative path for local image OR link to the image
+//STEP 1
+const images = [
+  {
+    url: "url",
+    altText: "altTest1",
+  },
+  {
+    url: "url",
+    altText: "altTest2",
+  },
+  {
+    url: "url",
+    altText: "altTest3",
+  },
+];
+
+// TODO: i need to create my thumbnail
+
+//TODO: i need to creat my large images
