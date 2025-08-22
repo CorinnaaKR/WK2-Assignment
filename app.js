35820141eed994e138fc86fd0c04cead6c5ed9f0
Corinna <x.corinna.x@hotmail.co.uk>
2025-08-22 11:39:11 +0100

    feat:add functions to create thumbnails. Following demo

--- app.js
+++ app.js
@@ -22,5 +22,15 @@ const images = [
 ];
 
 // TODO: i need to create my thumbnail
+//function createThumbnails(){
+//select DOM element (thumbnail container) to contain our thumbnails
+//this is a repetitive task ----> loop through through array using length property
+//inside the loop, we need
+//-create image element
+//-update the src and alt attributes to match those in the array(paraments)
+//give each image a className (.imgclassName)
+//add event listener to each image ---> the event handler of this listener is the function you will write to creat large images
+// - append the created images to the thumbnail-container
+//}
 
-//TODO: i need to creat my large images
+//TODO: i need to create my large images
